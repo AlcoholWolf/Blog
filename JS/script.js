88d768510bb5a15c7ceee1f71af96cdef28ac31c
늑이 <102508669+AlcoholWolf@users.noreply.github.com>
2022-09-05 11:47:32 +0900

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,12 @@
 const startButten = document.querySelector('start_butten')
 const loading = document.querySelector('.loading')
+
+$(document).ready(function(){
+    $('#slider_vol_x').mouseweheel(function(e,delta){
+        if ( delta > 0 ){
+            aa
+        } else if ( delta < 0 ){
+            aa
+        }
+    });
+});
